33e0753c0436a029c7b7183ef5127b514cabd146
fish119 <vvfish119@gmail.com>
2020-02-14 20:01:48 +0800

    Add Spring Cloud Config Server.(config client in module eureka-client-consumer)

---
+++ eureka-client-consumer/src/main/java/site/fish119/eurekaclientconsumer/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package site.fish119.eurekaclientconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Project SpringCloudDemo
+ * @Package site.fish119.eurekaclientconsumer.controller
+ * @Author fish119
+ * @Date 2020/2/14 19:12
+ * @Version V1.0
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
