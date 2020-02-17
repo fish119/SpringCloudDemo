258aa8bd3c98f356f500c6a11ce27effb7e7bdd8
fish119 <vvfish119@gmail.com>
2020-02-17 13:35:53 +0800

    Add : Spring Cloud Bus with RabbitMQ

---
+++ config-server/src/main/java/site/fish119/configserver/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package site.fish119.configserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Project SpringCloudDemo
+ * @Package site.fish119.configserver
+ * @Author fish119
+ * @Date 2020/2/17 13:26
+ * @Version V1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
