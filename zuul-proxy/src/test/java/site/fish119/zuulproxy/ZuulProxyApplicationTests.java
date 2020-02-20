6a37a254db34361e5877e1bc1ebb09c93d97c566
fish119 <vvfish119@gmail.com>
2020-02-20 13:00:46 +0800

    update .gitignore

---
+++ zuul-proxy/src/test/java/site/fish119/zuulproxy/ZuulProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package site.fish119.zuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
