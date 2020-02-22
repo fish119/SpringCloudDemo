7376f5a0717e03445b68db41f7c68016e2f7df86
fish119 <vvfish119@gmail.com>
2020-02-22 14:00:44 +0800

    update .gitignore

---
+++ admin-client/src/test/java/site/fish119/adminclient/AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package site.fish119.adminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
